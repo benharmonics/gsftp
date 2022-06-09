d408df91ba8cbf983c38ae49c0b9fb9dab3086e7
benharmonics <benjamin.jiron+git@gmail.com>
2022-06-08 23:08:56 -0600

    Added recursive downloading of files

--- src/file_transfer.rs
+++ src/file_transfer.rs
@@ -14,44 +14,42 @@ pub fn download_from_remote(sess: &Session, app: &App) -> Result<(), Box<dyn Err
     let target = app.buf.local.join(&app.content.remote[i]);
     let mut f = sftp.open(source.as_path()).expect("Failed to open file");
     if f.stat().expect("no stats").is_file() {
-        download_file_from_remote(sess, &mut f, &target)?;
+        download_file_from_remote(&mut f, &target)?;
+    } else {
+        download_directory_from_remote_recursive(&sftp, &source, &target)?;
     }
-    // let readdir_info = sftp.readdir(&source).unwrap_or_default();
-    // let children_of_source: Vec<&PathBuf> = readdir_info.iter().map(|(buf, _)| buf).collect();
-    // for (buf, stat) in &readdir_info {
-    //     if stat.is_file() {
-    //         download_file_from_remote(sess, &source, &target)?;
-    //     } else {
-    //         download_directory_from_remote_recursive(sess, &sftp, buf, &target)?;
-    //     }
-    // }
 
     Ok(())
 }
 
-fn download_file_from_remote(sess: &Session, source: &mut ssh2::File, target: &PathBuf) -> Result<(), Box<dyn Error>> {
-    let nbytes = &source.stat().expect("Failed to get file metatdata").size.unwrap();
-    let mut buffer = Vec::with_capacity(*nbytes as usize);
-    source.read_to_end(&mut buffer)?;
-    // let mut s = String::new();
-    // source.read_to_string(&mut s)?;
-    // write!(f, "{s}")?;
-    let mut f = fs::File::create(target.as_path()).unwrap();
-    f.write_all(&buffer).expect("Failed to write");
+fn download_file_from_remote(
+    source: &mut ssh2::File,
+    target: &PathBuf
+) -> Result<(), Box<dyn Error>> {
+    let nbytes: u64 = source.stat()?.size.unwrap_or_default();
+    let mut f = fs::File::create(target.as_path())?;
+    let mut buf = Vec::with_capacity(nbytes as usize);
+    source.read_to_end(&mut buf)?;
+    f.write_all(&buf)?;
 
     Ok(())
 }
 
 fn download_directory_from_remote_recursive(
-    sess: &Session,
     sftp: &Sftp,
-    buf: &PathBuf,
+    source: &PathBuf,
     target: &PathBuf
 ) -> Result<(), Box<dyn Error>> {
     fs::create_dir(&target).unwrap_or(());
-    let readdir_info = sftp.readdir(buf).unwrap_or_default();
+    let readdir_info = sftp.readdir(source).unwrap_or_default();
     for (buf, stat) in readdir_info {
-
+        let new_target = target.join(buf.file_name().unwrap());
+        if stat.is_dir() {
+            download_directory_from_remote_recursive(sftp, &buf, &new_target)?;
+        } else {
+            let mut f = sftp.open(buf.as_path())?;
+            download_file_from_remote(&mut f, &new_target)?;
+        }
     }
 
     Ok(())
