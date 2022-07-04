15368641efcaf3ace327cc70a4c5cf2a03b25be1
benharmonics <benjamin.jiron+git@gmail.com>
2022-07-04 00:24:05 -0600

    minor indentation issue

--- src/draw.rs
+++ src/draw.rs
@@ -28,8 +28,7 @@ pub fn ui<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) {
                     .split(f.size());
                 windows(f, chunks[0], app);
             }
-        })
-        .unwrap_or_else(|e| {
+        }).unwrap_or_else(|e| {
             eprintln!("Fatal error writing to terminal: {e}");
             std::process::exit(1);
         });
@@ -171,8 +170,7 @@ pub fn text_alert<B: Backend>(
                     right_aligned_text(f, chunks[1], t, s);
                 }
             }
-        })
-        .unwrap_or_else(|e| {
+        }).unwrap_or_else(|e| {
             eprintln!("Fatal error writing to terminal: {e}");
             std::process::exit(1);
         });
